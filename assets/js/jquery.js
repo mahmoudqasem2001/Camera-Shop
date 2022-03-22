42a1b2e7c2c7838329a68c4b86ecc308ad12f53e
mahmoud khaled <mahmoodqasem543@gmail.com>
2022-03-22 20:36:47 +0200

    My first commit

---
+++ assets/js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+   
+  });
